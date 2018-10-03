52ea02587db3327d3f3a935a69cf6241304db22a
Larry Gadea <trivex@gmail.com>
2018-10-03 09:13:55 -0700

    Initial Commit

---
+++ ThingsMenuApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ThingsMenuApp
+//
+//  Created by Larry Gadea on 10/3/18.
+//  Copyright © 2018 Larry Gadea. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
